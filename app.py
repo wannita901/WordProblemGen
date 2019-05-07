9d58c94662fe9b58be3e410ba91ea8be5978f857
Wannita901 <gam_wannita@hotmail..com>
2019-05-08 01:00:00 +0700

    add Jinja2 and CSS

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, url_for, make_response
 from keras.preprocessing import sequence
 from keras import backend as K
 import numpy as np
+import os 
+import json
+
 app = Flask(__name__)
 
 from generate import gen_auto
@@ -26,12 +29,26 @@ def problem_generate():
     cnn_model._make_predict_function()
     test_pred = cnn_model.predict(np.array(testset))
     test_pred_t = prob_to_class(test_pred)
-    output = ""
+    output = '{"problems":['
     dictionary, reverse_dictionary = load_object('dict.pl')
     for i in range(len(test_pred_t)):
         if test_pred_t[i]==0:
             continue
-        output+="<p>" + str(("").join([reverse_dictionary[x] for x in testset[i] if x!=0]).strip())
-        output+= '\n ans:' + str(data['answers'][i]) + "\n\n" + "</p>"
+        output+= '{"pb":"' + str(("").join([reverse_dictionary[x] for x in testset[i] if x!=0]).strip())
+        output+= '","ans":"' + str(data['answers'][i]) + '"},'
+    output = output[:-1] + ']}'
     K.clear_session()
-    return output
+    return render_template('main.html', output = output)
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                 endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
